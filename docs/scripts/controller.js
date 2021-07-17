1429cd9dedfc61f77baed4918f6f4bf3b8f0e523
Rodolfo Lourenzutti <rodolfo.lourenzutti@gmail.com>
2021-07-17 12:29:21 -0700

    Initial commit: STAT 450

---
+++ docs/scripts/controller.js
@@ -0,0 +1,48 @@
+// Add data to the current page history
+//history.replaceState({ page: "index.html" }, "", "index.html")
+
+// load the page with the back button
+window.onpopstate = function (event) {
+    load_page(event.state.page);
+}
+
+// loading the main tag
+function load_page(path, path_js) {
+    const page = path.split("#")[0];
+    const section = path.split("#")[1];
+
+    fetch(page)
+        .then(response => response.text())
+        .then(text => {
+            const parser = new DOMParser();
+            const html = parser.parseFromString(text, "text/html");
+            text = html.querySelector("main").innerHTML;
+            document.querySelector('main').innerHTML = text;
+        })
+        .then(() =>  {
+            if (path_js) load_js(path_js);
+        });
+}
+
+
+function load_js(path){
+    script = document.createElement("script")
+    script.src = path;
+    document.head.append(script);
+}
+
+// add event listener to load the pages.
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelectorAll('[data-page]').forEach(link => {
+        link.onclick = (event) => {
+            load_page(link.dataset.page, link.dataset.js);
+            history.pushState({ page: link.dataset.page }, "", link.dataset.page)
+            return false;
+        };
+    });
+});
+
+
+
+
+
